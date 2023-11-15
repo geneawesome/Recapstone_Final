c446c49c7a8843280bde2618b1dbdda5edc0197f
geneawesome <genecolumna@gmail.com>
2023-11-15 23:24:18 +0800

    change in background in about us

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -56,52 +56,6 @@ const Footer = () => {
         </div>
       </div>
     </footer>
-
-    //     <footer class="footer-distributed">
-
-    //         <div class="footer-left">
-    //             <h3>Contact<span> Us</span></h3>
-
-    //             <p class="footer-links">
-    //                 <a href="#">Email</a>
-    //                 <a href="#">Account</a>
-    //                 <a href="#">Subcribe</a>
-    //                 <a href="#">Blog</a>
-    //             </p>
-
-    //             <p class="footer-company-name">Copyright © 2023 All rights reserved</p>
-    //         </div>
-
-    //         <div class="footer-center">
-    //             <div>
-    //                 <i class="fa fa-map-marker"></i>
-    //                 <p><span>790 P. Ocampo Sr. St.</span>
-    //                 (formerly, Ocampo St, Malate, Manila, 1004 Metro Manila)</p>
-    //             </div>
-
-    //             <div>
-    //                 <i class="fa fa-phone"></i>
-    //                 <p>096763179108</p>
-    //             </div>
-    //             <div>
-    //                 <i class="fa fa-envelope"></i>
-    //                 <p><a href="#">Gyminfinity@gmail.com</a></p>
-    //             </div>
-    //         </div>
-    //         <div class="footer-right">
-    //             <p class="footer-company-about">
-    //                 <span>Social Media</span>
-    //                 <strong>Contact me at my Social Media Accounts</strong>
-
-    //             </p>
-    //             <div class="footer-icons">
-    //                 <a href="#"><i class="fa fa-facebook"></i></a>
-    //                 <a href="#"><i class="fa fa-instagram"></i></a>
-    //                 <a href="#"><i class="fa fa-twitter"></i></a>
-    //             </div>
-    //         </div>
-
-    // </footer>
   );
 };
 
