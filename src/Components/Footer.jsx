0eb93b359d153036a971e2f07748fe8581032bbe
geneawesome <genecolumna@gmail.com>
2023-11-17 03:46:25 +0800

    remove the modal that is un used

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,16 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Footer.css";
-import { useState } from "react";
-import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
-
 const Footer = () => {
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
   return (
     <footer className="footer">
       <div className="container">
